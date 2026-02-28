56c9cedf8332b5f95cb6436b0fb297c17e79a324
hstdt <hstdt@users.noreply.github.com>
2026-02-28 16:03:12 +0800

    Globally unified animation effects

--- Sources/UnionShake/ShakeContainer.swift
+++ Sources/UnionShake/ShakeContainer.swift
@@ -6,6 +6,29 @@
 //
 
 import SwiftUI
+import Combine
+
+@MainActor
+class ShakeContainerObservation: ObservableObject {
+    static let shared = ShakeContainerObservation()
+    var cancellables = Set<AnyCancellable>()
+    @Published var version: CGFloat = 0
+    private let versionSubject = PassthroughSubject<Void, Never>()
+
+    private init() {
+        versionSubject
+            .debounce(for: .seconds(0.05), scheduler: DispatchQueue.main)
+            .sink { [weak self] in
+                guard let self else { return }
+                version += 1
+            }
+            .store(in: &cancellables)
+    }
+
+    func versionIncrease() {
+        versionSubject.send()
+    }
+}
 
 struct ShakeContainer<T: Equatable, Content: View>: View {
     let trigger: T
@@ -13,16 +36,21 @@ struct ShakeContainer<T: Equatable, Content: View>: View {
     let enabled: Bool
     let shakesPerUnit: CGFloat
     let content: Content
-
-    @State private var version: CGFloat = 0
+    @StateObject private var shake = ShakeContainerObservation.shared
+    @State private var animation: Bool = false
 
     var body: some View {
         content
-            .modifier(ShakeEffect(amount: amount, shakesPerUnit: shakesPerUnit, animatableData: version))
+            .modifier(ShakeEffect(amount: amount, shakesPerUnit: shakesPerUnit, animatableData: shake.version))
+            .animation(animation ? .default : .none, value: shake.version)
             .onChange(of: trigger) { _ in
                 guard enabled else { return }
-                withAnimation {
-                    version += 1
+                animation = true
+                shake.versionIncrease()
+            }
+            .onChange(of: shake.version) { _ in
+                DispatchQueue.main.async {
+                    animation = false
                 }
             }
     }
